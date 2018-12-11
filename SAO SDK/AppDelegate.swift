32228e7024cd7be22a45e2d92376b8cc10ef7707
Alessandro Mascolo <manoplay@imac-di-alessandro.local>
2018-12-11 18:28:17 +0100

    Initial Commit

---
+++ SAO SDK/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SAO SDK
+//
+//  Created by Alessandro Mascolo on 12/11/18.
+//  Copyright © 2018 Alessandro Mascolo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
